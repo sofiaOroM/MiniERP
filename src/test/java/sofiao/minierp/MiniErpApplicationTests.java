0b75cc921bef07fc48d37c25051d76a5b83ac423
Sofia Orozco <sofiamen288@gmail.com>
2026-09-13 18:29:27 -0600

    Inicial

---
+++ src/test/java/sofiao/minierp/MiniErpApplicationTests.java
@@ -0,0 +1,13 @@
+package sofiao.minierp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
